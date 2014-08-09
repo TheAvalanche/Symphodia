734a517626ac5bd373a727f50ebcd621367888c8
Alex Kartishev <alexkartishev@gmail.com>
2014-08-09 18:06:14 +0300

    News edit icon

--- admin/admin-war/src/main/webapp/ng/js/controllers/band/ListNewsCtrl.js
+++ admin/admin-war/src/main/webapp/ng/js/controllers/band/ListNewsCtrl.js
@@ -2,7 +2,16 @@
     'use strict';
 
     angular.module('adminApp.controllers')
-        .controller('ListNewsCtrl', ['$scope', '$modal', function ($scope, $modal) {
+        .controller('ListNewsCtrl', ['$scope', '$modal', 'NewsService', function ($scope, $modal, NewsService) {
+
+            NewsService.getAll().
+                success(function (data, status, headers, config) {
+                    $scope.newsList = data;
+                }).
+                error(function (data, status, headers, config) {
+                    $scope.newsList = [];
+                });
+
             $scope.openAddNews = function() {
                 var modalInstance = $modal.open({
                     templateUrl: '/admin/ng/partials/band/edit-news.html',
