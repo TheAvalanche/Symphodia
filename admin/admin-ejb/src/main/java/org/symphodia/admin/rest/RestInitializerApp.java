c1755b9ea3e9cab6d1c74855a010a6ff07f7d1c9
Alex Kartishev <alexkartishev@gmail.com>
2014-08-12 16:40:09 +0300

    Dependencies

---
+++ admin/admin-ejb/src/main/java/org/symphodia/admin/rest/RestInitializerApp.java
@@ -0,0 +1,8 @@
+package org.symphodia.admin.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class RestInitializerApp extends Application {
+}
