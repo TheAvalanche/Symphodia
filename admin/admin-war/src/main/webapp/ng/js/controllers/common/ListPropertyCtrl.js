e2e1680784cfe3aa430eaacfc1e657640f74ba15
Alex Kartishev <alexkartishev@gmail.com>
2014-08-18 15:11:27 +0300

    Facebook properties added

--- admin/admin-war/src/main/webapp/ng/js/controllers/common/ListPropertyCtrl.js
+++ admin/admin-war/src/main/webapp/ng/js/controllers/common/ListPropertyCtrl.js
@@ -16,6 +16,10 @@
                 });
             };
 
+            $scope.toTitle = function(text) {
+                return text.split("_").join(" ").replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+            };
+
             init();
         }]);
 }());
