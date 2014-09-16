fbb78d1e4f2df4c23601e3c7aab19ff33ffbefe5
Alex Kartishev <alexkartishev@gmail.com>
2014-09-16 13:30:33 +0300

    Project restructure

--- band/band-ejb/src/main/java/org/symphodia/band/rest/RestInitializerApp.java
+++
@@ -1,8 +0,0 @@
-package org.symphodia.band.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class RestInitializerApp extends Application {
-}
